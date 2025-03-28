7c44dc0baf06e37e238347efc4daed176a040ed7
Kamthegabru <154924574+Kamthegabru@users.noreply.github.com>
2025-03-27 21:49:27 -0700

    Update Landing.js

--- src/Component/ClientSide/BeforeLogin/Landing.js
+++ src/Component/ClientSide/BeforeLogin/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button } from "@mui/material";
+/*import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+*/
 import { Card, CardContent, Typography, Grid, Container, CardMedia } from "@mui/material";
 
 function Landing() {
